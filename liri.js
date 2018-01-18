e405729c4537072cada33b767bfcf1fa18798615
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2018-01-18 01:29:53 -0800

    add access keys to twitter

--- liri.js
+++ liri.js
@@ -5,11 +5,16 @@ require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
 
+var request = require("request");
+
 var Twitter = require("twitter");
 
 var Spotify = require("node-spotify-api");
 
-var request = require("request");
+
+ var spotify = new Spotify(keys.spotify);
+ var client = new Twitter(keys.twitter);
+
 
 var command = process.argv[2]
 var userInput = process.argv[3]
@@ -17,7 +22,8 @@ var userInput = process.argv[3]
 
 //Twitter function
 
-var params = {screen_name: 'louellalandicho};
+
+var params = {screen_name: 'louellalandicho'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     console.log(tweets);
