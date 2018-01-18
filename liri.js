5af46bab3522429c4f6b586e5eb0f5f151986429
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2018-01-18 02:53:58 -0800

    add do what it says

--- liri.js
+++ liri.js
@@ -20,6 +20,9 @@ var command = process.argv[2]
 var userInput = process.argv[3]
 
 
+
+
+
 //Twitter function
 
 var params = {screen_name: 'louellalandicho'};
@@ -45,24 +48,20 @@ spotify
 
 
 //Movie function
-function movieThis(movie) {
-	// body...
-	// console.log('movieThis works')
-	// console.log('searching ombd for: '+movie)
-    var omdbURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=f1a4421f"
-    request(omdbURL, function (error, response, body){
-    	var data = JSON.parse(body)
-    	// console.log(data)
-    	console.log('Title: '+data.Title)
-    	console.log('Year: '+data.Year)
-    	console.log('IMDB rating: '+data.imdbRating)
-    	console.log('Rotten Tomatoes rating: '+data.Ratings[1].Value)
-    	console.log('Country: '+data.Country)
-    	console.log('Language: '+data.Language)
-    	console.log('Plot: '+data.Plot)
-    	console.log('Actors: '+data.Actors)
-	})
-};
+
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+    fs.writeFile("response.txt", JSON.stringify(response), function(err){
+      console.log('error', error);
+    });
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  }
+});
+
 
 
 
