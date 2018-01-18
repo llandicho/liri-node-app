6ac68b737b3c6aea2cdad5fad8dcb67f7a5f4f13
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2018-01-18 02:34:27 -0800

    add movie omdb

--- liri.js
+++ liri.js
@@ -21,6 +21,7 @@ var userInput = process.argv[3]
 
 
 //Twitter function
+
 var params = {screen_name: 'louellalandicho'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
@@ -28,8 +29,9 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   }
 });
 
-//Spotify function
 
+
+//Spotify function
 spotify
   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
   .then(function(data) {
@@ -38,4 +40,50 @@ spotify
   .catch(function(err) {
     console.error('Error occurred: ' + err); 
   });
+
+
+
+
+//Movie function
+function movieThis(movie) {
+	// body...
+	// console.log('movieThis works')
+	// console.log('searching ombd for: '+movie)
+    var omdbURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=f1a4421f"
+    request(omdbURL, function (error, response, body){
+    	var data = JSON.parse(body)
+    	// console.log(data)
+    	console.log('Title: '+data.Title)
+    	console.log('Year: '+data.Year)
+    	console.log('IMDB rating: '+data.imdbRating)
+    	console.log('Rotten Tomatoes rating: '+data.Ratings[1].Value)
+    	console.log('Country: '+data.Country)
+    	console.log('Language: '+data.Language)
+    	console.log('Plot: '+data.Plot)
+    	console.log('Actors: '+data.Actors)
+	})
+};
+
+
+
+//Do it function
+
+function doIt() {
+  	// We will read the existing random.txt file
+  	fs.readFile("random.txt", "utf8", function(err, data) {
+	    if (err) {
+	      return console.log(err);
+	    }
+		// separate data into command and userInput
+		data = data.split(",");
+		// console.log(data)
+		command = data[0]
+		userInput = data[1]
+		// console.log('command: '+command, 'userInput: ' +userInput)
+		console.log('you want me to run '+command+' on '+userInput)
+		runCommand(command)
+	})
+};
+
+
 
